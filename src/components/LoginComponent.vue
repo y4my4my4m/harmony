f46789f67daf35c6a34aa85c0406b35d2a253583
y4my4my4m <8145020+y4my4my4m@users.noreply.github.com>
2024-01-19 02:44:39 -0500

    styling

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -6,7 +6,6 @@
         <h2>Login</h2>
         <input v-model="email" type="email" placeholder="Email" />
         <input v-model="password" type="password" placeholder="Password" />
-        <br/>
         <div class="buttons">
           <button class="login" @click="login">Login</button>
           <button class="register" @click="$router.push('/register')">Register</button>
@@ -87,7 +86,7 @@ export default defineComponent({
   .login-bg{
     /* background: rgba(0,0,0,0.93); */
     background: linear-gradient(var(--bg-rotation-angle), rgba(0,0,0,.9) 0%, rgba(0,0,0,0.7) 100%);
-    padding: 40px 140px 100px 140px; 
+    padding: 40px 120px 100px 120px; 
     position: relative;
     z-index:5;
     border-radius:24px;
@@ -97,7 +96,7 @@ export default defineComponent({
     text-align: center;
     margin: 0px auto;
     font-weight:900;
-    font-size: 12em;
+    font-size: 9em;
     /* font-family: 'Rubik Moonrocks', sans-serif; */
     font-family: 'Arizonia', cursive;
     /* font-family: 'Press Start 2P', system-ui; */
@@ -108,15 +107,15 @@ export default defineComponent({
   h2 {
     text-align: center;
     font-weight:100;
-    font-size:3em;
+    font-size:2em;
     position:relative;
-    top: -30px;
+    top: -15px;
   }
   .logo {
     display:block;
-    margin: 20px auto;
-    width: 12rem;
-    height: 12rem;
+    margin: 15px auto;
+    width: 8rem;
+    height: 8rem;
     position:relative;
     top: 50px;
     cursor: pointer;
@@ -138,7 +137,7 @@ export default defineComponent({
     background-color: #2f3136; /* Slightly lighter than the background */
     color: white; /* White text */
     outline: none;
-    font-size:2em;
+    font-size:1em;
   }
   input:focus{
     outline: none;
@@ -149,7 +148,7 @@ export default defineComponent({
     -webkit-box-shadow:0 0 0 50px #24274e inset; /* Change the color to your own background color */
     border: 1px solid #242b37; /* Subtle border */
     -webkit-text-fill-color: #dddddd;
-    font-size:2em;
+    font-size:1em;
   }
 
   input:-webkit-autofill:focus {
@@ -164,15 +163,15 @@ export default defineComponent({
   button {
     display: block;
     width: 100%;
-    margin: 20px auto;
-    padding: 20px;
+    margin: 15px auto;
+    padding: 15px;
     border: none;
     border-radius: 6px;
     color: white;
     cursor: pointer;
     background: rgba(128,128,128,0.2);
     transition: .3s;
-    font-size:1.75em;
+    font-size:1.15em;
     font-weight:bold;
   }
   button.login {
