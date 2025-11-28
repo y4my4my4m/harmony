dd1b60a719a456eb46b100813e5a9fc9fa438df3
y4my4my4m <8145020+y4my4my4m@users.noreply.github.com>
2025-11-28 17:29:27 +0900

    fix

--- src/components/common/Icon.vue
+++ src/components/common/Icon.vue
@@ -55,10 +55,10 @@
     <path v-if="name === 'headphones'" d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zm-14 0a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3z"></path>
     
     <!-- Headphones Off -->
-    <path v-if="name === 'headphones-off'" d="M21 14h-1.343a2 2 0 0 0-1.414.586L17 16.828V19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 .586-1.414L14.828 13"></path>
-    <path v-if="name === 'headphones-off'" d="M7 14H4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2z"></path>
     <line v-if="name === 'headphones-off'" x1="1" y1="1" x2="23" y2="23"></line>
-    
+    <path v-if="name === 'headphones-off'" d="M3 18v-6a9 9 0 0 1 18 0v6"></path>
+    <path v-if="name === 'headphones-off'" d="M21 19a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3zm-14 0a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3z"></path>
+
     <!-- Phone -->
     <path v-if="name === 'phone'" d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
     
